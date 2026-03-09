54eaf94f50286f275a1704f7e67d7c33153e6107
ernestakh777-prog <ernestakh777@gmail.com>
2026-03-09 14:59:36 +0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const PORT = 3000;
+
+const CLIENT_ID = "GPLs2EJHBWrE8UT4fbmUpHWxgEAJzacX";
+const CLIENT_SECRET = "tAX9BHGKdD3u3JZy";
+
+let accessToken = null;
+
+async function getToken() {
+  const response = await fetch(
+    "https://test.api.amadeus.com/v1/security/oauth2/token",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body:
+        "grant_type=client_credentials&client_id=" +
+        CLIENT_ID +
+        "&client_secret=" +
+        CLIENT_SECRET,
+    }
+  );
+
+  const data = await response.json();
+  accessToken = data.access_token;
+}
+
+app.get("/flights", async (req, res) => {
+  const { origin, destination, date } = req.query;
+
+  if (!accessToken) {
+    await getToken();
+  }
+
+  const url =
+    `https://test.api.amadeus.com/v2/shopping/flight-offers` +
+    `?originLocationCode=${origin}` +
+    `&destinationLocationCode=${destination}` +
+    `&departureDate=${date}` +
+    `&adults=1&max=5`;
+
+  const response = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  const data = await response.json();
+  res.json(data);
+});
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
